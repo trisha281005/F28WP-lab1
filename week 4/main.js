34eb016cb0082f7cd0d90130e3422dc1a5884f9d
trisha281005 <145096178+trisha281005@users.noreply.github.com>
2023-11-09 16:01:07 +0400

    Update main.js

--- week 4/main.js
+++ week 4/main.js
@@ -2,5 +2,6 @@ var ourRequest = new XMLHttpRequest();
 ourRequest.open('GET', 'https://trisha281005.github.io/F28WP-lab1/week%204/cities1.json');
 ourRequest.onload = function() {
 console.log(ourRequest.responseText);
+  console.log(ourData[0]);
 };
 ourRequest.send();
