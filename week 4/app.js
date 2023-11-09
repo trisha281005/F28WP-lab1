d0496039009301afb4203217030d33b79cfa10ed
trisha281005 <145096178+trisha281005@users.noreply.github.com>
2023-11-09 18:43:00 +0400

    Update app.js

--- week 4/app.js
+++ week 4/app.js
@@ -1,47 +1,40 @@
-// Step 1
-const apiKey = "ea3bc5d58e1a5432c4dc0cfcb3fcb65b"; 
+var apiKey = 'ea3bc5d58e1a5432c4dc0cfcb3fcb65b'; 
+var weatherContainer = document.getElementById("weather-info");
+var btn = document.getElementById("btn");
 
-// Step 2
-const cityInput = document.getElementById("cityInput");
-const btn = document.getElementById("btn");
-const weatherInfo = document.getElementById("weather-info");
-
-// Step 3
-btn.addEventListener("click", () => {
-  // Step 4
-  const city = cityInput.value;
-
-  if (city === "") {
-    alert("Please enter a city name.");
+btn.addEventListener("click", function() {
+  var city = document.getElementById('cityInput').value;
+  if (!city) {
+    alert("Please enter a city name!");
     return;
   }
 
-  // Step 5
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  var ourRequest = new XMLHttpRequest();
+  ourRequest.open('GET', 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&appid=' + apiKey);
 
-  fetch(apiUrl)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
+  ourRequest.onload = function() {
+    if (ourRequest.status >= 200 && ourRequest.status < 400) {
+      var ourData = JSON.parse(ourRequest.responseText);
+      if (ourData.cod === 200) {
+        renderWeather(ourData);
+      } else {
+        alert("City not found. Please try again.");
       }
-      return response.json();
-    })
-    .then((data) => {
-      // Step 7
-      const weatherDescription = data.weather[0].description;
-      const temperature = data.main.temp;
-      const windSpeed = data.wind.speed;
+    } else {
+      alert("We connected to the server, but it returned an error.");
+    }
+  };
 
-      const weatherHTML = `
-        <p>The weather in ${city} is ${weatherDescription}</p>
-        <p>The temperature is  ${temperature}°C with a wind speed of  ${windSpeed} m/s </p>
-      `;
+  ourRequest.onerror = function() {
+    console.log("Connection error");
+  };
 
-      weatherInfo.innerHTML = weatherHTML;
-    })
-    .catch((error) => {
-      // Step 6
-      console.error("Error:", error);
-      weatherInfo.innerHTML = "Error fetching weather data.";
-    });
+  ourRequest.send();
 });
+
+function renderWeather(data) {
+  var htmlString = "<p>The weather in " + data.name + " is " + data.weather[0].description + 
+                   ".</br> The temperature is " + data.main.temp + "°C with a wind speed of " +
+                   data.wind.speed + "m/s. <hr></p>";
+  weatherContainer.insertAdjacentHTML('beforeend', htmlString);
+}
