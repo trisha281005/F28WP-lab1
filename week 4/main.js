b61aa8226e445595dcef1d9cd4c98ef9cacc8993
trisha281005 <145096178+trisha281005@users.noreply.github.com>
2023-11-09 16:04:11 +0400

    part1 of json main.js

--- week 4/main.js
+++ week 4/main.js
@@ -1,7 +1,19 @@
+var cityContainer = document.getElementById("city-info");
+var btn = document.getElementById("btn");
+btn.addEventListener("click", function(){
 var ourRequest = new XMLHttpRequest();
 ourRequest.open('GET', 'https://trisha281005.github.io/F28WP-lab1/week%204/cities1.json');
 ourRequest.onload = function() {
 var ourData = JSON.parse(ourRequest.responseText);
-console.log(ourData[0]);
+renderHTML(ourData);
 };
 ourRequest.send();
+});
+function renderHTML(data){
+var htmlString = "";
+for (i=0; i<data.length; i++){
+htmlString += "<p>" + data[i].name + " is a city in " + data[i].country + ".</p>"
+;
+}
+cityContainer.insertAdjacentHTML('beforeend' , htmlString);
+}
